fa727b81ebbe2548c0089782fccfca06d97e4485
gyzboy <gyzboy@126.com>
2017-05-12 14:37:09 +0800

    增加启动优化代码

--- androidopensamples/src/main/java/com/gyz/androidopensamples/optimization/startup/InitializeService.java
+++ androidopensamples/src/main/java/com/gyz/androidopensamples/optimization/startup/InitializeService.java
@@ -1,8 +1,38 @@
 package com.gyz.androidopensamples.optimization.startup;
 
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by gyzboy on 2017/5/10.
  */
 
-public class InitializeService {
+public class InitializeService extends IntentService {
+
+    private static final String ACTION_INIT_WHEN_APP_CREATE = "com.gyz.androidsamples.service.action.INIT";
+
+    public InitializeService() {
+        super("InitializeService");
+    }
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, InitializeService.class);
+        intent.setAction(ACTION_INIT_WHEN_APP_CREATE);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_INIT_WHEN_APP_CREATE.equals(action)) {
+                performInit();
+            }
+        }
+    }
+
+    private void performInit() {
+        //可以在异步线程中初始化的一些操作
+    }
 }
--- androidopensamples/src/main/java/com/gyz/androidopensamples/optimization/startup/LogoActivity.java
+++ androidopensamples/src/main/java/com/gyz/androidopensamples/optimization/startup/LogoActivity.java
@@ -1,8 +1,24 @@
 package com.gyz.androidopensamples.optimization.startup;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by gyzboy on 2017/5/10.
  */
 
-public class LogoActivity {
+public class LogoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //if (firstRun) {
+        //    //logo空界面
+        //}
+        //else {
+        //   //app主界面
+        //}
+        finish();
+    }
 }
